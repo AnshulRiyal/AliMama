fde97c52d51d80d1f30cddd9f60573c89ac94755
AnshulRiyal <anshulriyal00@gmail.com>
2021-02-23 01:16:41 +0530

    Create controller for order_items and routes for cart and items in cart.

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,10 @@
+class OrderItemsController < ApplicationController
+	def index
+		render html: "Index Cart page"
+	end
+
+	def create
+		
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+	root to: 'categories#index'
+
+	resources :categories, only: [:index] do
+		resources :products, only: [:index]
+	end
+
+	resources :order_items, path: '/cart/items'
+	get '/cart', to: 'order_items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
